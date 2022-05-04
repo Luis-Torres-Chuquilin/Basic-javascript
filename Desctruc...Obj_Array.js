1b6583155ed90fdf1ce28ae15032de474d973346
Luis Torres <luistorres@Luiss-MacBook-Pro.local>
2022-05-05 08:25:13 +1000

    L

---
+++ Desctruc...Obj_Array.js
@@ -0,0 +1,20 @@
+/** @format */
+
+// OBJECT
+let payload = { cantante: "Luis" };
+
+const post = "doctor";
+payload = { ...payload, post };
+
+console.log(payload); // { post: 'doctor'}
+
+const { cantante } = payload;
+
+console.log(cantante);
+
+// ARRAY;
+let payload2 = [];
+
+let l1 = [1, 2];
+l1 = [...l1, "camaleon"];
+console.log("l3", l1);
